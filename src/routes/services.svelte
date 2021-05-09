c06927b06e455005835ec276f4d19bbf3ccca442
1983shiv <1983.shiv>
2021-05-09 15:47:10 +0530

    Updates

--- src/routes/services.svelte
+++
@@ -1,9 +0,0 @@
-<section class="text-gray-600 body-font overflow-hidden">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -m-12">
-      <div class="p-12 md:w-1/2 flex flex-col items-start">
-        <h2>This is Services Page</h2>
-      </div>
-    </div>
-  </div>
-</section>
